477d945791b158604598694caf35e42b5a4e72e9
nickmaria06 <131277530+nickmariaa06@users.noreply.github.com>
2023-10-11 12:26:24 -0300

    Add files via upload

---
+++ 2-operaçõesAritmeticas.js
@@ -0,0 +1,6 @@
+2-operaçõesAritmeticas
+console.log("operaçõeAritmeticas.js");
+console.log(2+2);
+console.log(10+8);
+console.log(10+8*2);
+console.log("ano"+2020);
---
+++ meuPrimeiroPrograma.js
@@ -0,0 +1,6 @@
+console.log("meuPrimeiroPrograma")
+const idade=17
+console.log(idade);
+console.log(idade+2);
+const idadeSomada=idade+2
+console.log(idadeSomada);
